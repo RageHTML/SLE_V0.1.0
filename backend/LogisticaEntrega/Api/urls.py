50ca788fab56ecb56ecb081fb2d450375c267843
RageHTML <deyvidm24@gmail.com>
2026-09-20 15:59:52 +0000

    feat: organizar estrutura

--- backend/LogisticaEntrega/Api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('pedidos/', views.order_manager, name='gerenciar_pedidos'),
-    path('pedidos/<int:pk>/', views.order_manager, name='detalhar_pedido'),
-]
